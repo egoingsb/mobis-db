13c0492bb1f88582862e34b5b52382e97d58bb8c
egoing <egoing@gmail.com>
2019-11-26 10:02:05 +0900

    반복문을 이용해서 여러행을 처리하기

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-Hello PHP!
 <?php
-print(rand());
+$conn1 = mysqli_connect('127.0.0.1', 'root', '111111', 'app');
+$sql = "SELECT * FROM topic";
+$result1 = mysqli_query($conn1, $sql);
 ?>
-END
+<h1>DATABASE</h1>
+<h2>MySQL</h2>
+<ul>
+	<?php
+	while($row = mysqli_fetch_array($result1)){
+		print("<li>".$row['title']."</li>");
+	}
+	?>
+</ul>
+<p>by egoing</p>
+<p><a href="http://mysql.com">MySQL</a> is ...</p>
