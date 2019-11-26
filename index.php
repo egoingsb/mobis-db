aa0ee06b20ab0152aa370cb4c99611ce3fbd7a61
egoing <egoing@gmail.com>
2019-11-26 10:13:48 +0900

    글 상세보기 출력

--- index.php
+++ index.php
@@ -2,15 +2,21 @@
 $conn1 = mysqli_connect('127.0.0.1', 'root', '111111', 'app');
 $sql = "SELECT * FROM topic";
 $result1 = mysqli_query($conn1, $sql);
+
+$sql = "SELECT * FROM topic WHERE id = ".$_GET['id'];
+$result2 = mysqli_query($conn1, $sql);
+$article = mysqli_fetch_array($result2);
 ?>
 <h1>DATABASE</h1>
-<h2>MySQL</h2>
+<h2>
+<?=$article['title']?>
+</h2>
 <ul>
-	<?php
-	while($row = mysqli_fetch_array($result1)){
-		print("<li>".$row['title']."</li>");
-	}
-	?>
+<?php
+while($row = mysqli_fetch_array($result1)){
+	print("<li>".$row['title']."</li>");
+}
+?>
 </ul>
-<p>by egoing</p>
-<p><a href="http://mysql.com">MySQL</a> is ...</p>
+<p>by <?=$article['author']?></p>
+<p><?=$article['description']?></p>
