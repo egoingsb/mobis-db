a0e15daac575fe7e7934a087b820bb7f67702192
egoing <egoing@gmail.com>
2019-11-27 13:20:38 +0900

    글 추가 기능 완성

--- create_process.php
+++ create_process.php
@@ -1,16 +1,24 @@
 <?php
-$conn1 = mysqli_connect('127.0.0.1', 'root', '111111', 'app');
+$conn1 = mysqli_connect('127.0.0.1', 'root', '111111', 'app3');
 $sql = "
     INSERT INTO topic
-        (title, description, author, created)
+        (title, description, created)
         VALUES(
             '{$_POST['title']}',
             '{$_POST['description']}',
-            '{$_POST['author']}',
             NOW()
         )
 ";
 $result1 = mysqli_query($conn1, $sql);
 $inserted_id = mysqli_insert_id($conn1);
+$sql = "
+INSERT INTO topic_has_author 
+    (topic_id, author_id)
+    VALUES(
+        {$inserted_id}, 
+        {$_POST['author_id']}
+    )
+";
+mysqli_query($conn1, $sql);
 header("Location:http://localhost/index.php?id=".$inserted_id);
 ?>
