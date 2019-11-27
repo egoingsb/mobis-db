0932576a176e87f0a084162dcf6aaa84bf353f17
egoing <egoing@gmail.com>
2019-11-27 11:05:12 +0900

    저자 목록 출력

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$conn1 = mysqli_connect('127.0.0.1', 'root', '111111', 'app');
+$conn1 = mysqli_connect('127.0.0.1', 'root', '111111', 'app3');
 $sql = "SELECT * FROM topic";
 $result1 = mysqli_query($conn1, $sql);
 
@@ -18,6 +18,25 @@ while($row = mysqli_fetch_array($result1)){
 }
 ?>
 </ul>
-<p>by <?=$article['author']?></p>
+<?php
+$sql = "
+SELECT 
+    A.id AS author_id, A.name, A.profile
+FROM
+    topic_has_author AS THA
+		LEFT JOIN
+	author AS A ON THA.author_id = A.id
+WHERE
+    topic_id = {$_GET['id']};
+";
+$result3 = mysqli_query($conn1, $sql);
+?>
+<ul>
+<?php
+while($authors = mysqli_fetch_array($result3)){
+	print("<li>{$authors['name']}</li>");
+}
+?>
+</ul>
 <p><?=$article['description']?></p>
 <p><a href="create.php">create</a></p>
